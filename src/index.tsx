aedb99abdd8eb900568575a5f9629310db946efb
Dick Hon <honchunkin@gmail.com>
2023-07-02 17:36:33 +0800

    refactor: lint project

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,9 @@ import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <MantineProvider withGlobalStyles withNormalizeCSS>
